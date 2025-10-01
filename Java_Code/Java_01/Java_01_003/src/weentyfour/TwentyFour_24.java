82ddff8e365edbb2aca50c0df57e11cf6618de8b
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-01 16:39:09 +0530

    Day 24 Completed

---
+++ Java_Code/Java_01/Java_01_003/src/weentyfour/TwentyFour_24.java
@@ -0,0 +1,61 @@
+package weentyfour;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TwentyFour_24 {
+    public static void main(String[] args) {
+//        String s1 = "ab";
+//        String s2 = "eidbaooo";
+//        System.out.println(checkInclusion(s1, s2));
+        int[] a = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(a));
+
+    }
+
+    public static int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = currentHeight * width;
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] s1Freq = new int[26];
+        int[] s2Freq = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            s1Freq[s1.charAt(i) - 'a']++;
+            s2Freq[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(s1Freq, s2Freq)) return true;
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            s2Freq[s2.charAt(i) - 'a']++;
+            s2Freq[s2.charAt(i - s1.length()) - 'a']--;
+            if (Arrays.equals(s1Freq, s2Freq)) return true;
+        }
+
+        return false;
+    }
+
+
+}
