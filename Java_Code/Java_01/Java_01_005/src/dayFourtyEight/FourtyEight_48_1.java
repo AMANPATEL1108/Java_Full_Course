3367d6fa365ce442906ec331e01768f4d911c281
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-10-25 10:48:31 +0530

    Completed Day 48

---
+++ Java_Code/Java_01/Java_01_005/src/dayFourtyEight/FourtyEight_48_1.java
@@ -0,0 +1,51 @@
+package dayFourtyEight;
+
+public class FourtyEight_48_1 {
+
+
+    public static void main(String[] args) {
+        Counter counter=new Counter();
+        MyThread t1=new MyThread(counter);
+        MyThread t2=new MyThread(counter);
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println(counter.getCount());
+
+
+    }
+}
+
+class Counter{
+    private int count=0;
+    public  void increment(){
+        synchronized (this){   //one instance also other way to in object method in that add  text like " public synchronized void increment(){"
+            count++;
+        }
+        count++;
+    }
+    public int getCount(){
+        return count;
+    }
+}
+
+class MyThread extends  Thread{
+    private  Counter counter;
+    public  MyThread(Counter counter){
+        this.counter=counter;
+    }
+
+    @Override
+    public  void run(){
+        for(int i=0;i<1000;i++){
+            counter.increment();
+        }
+    }
+
+}
