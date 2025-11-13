69a0b9b50ab0c4928ce5afc71b5656ebe34c8aef
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-11-13 08:27:47 +0530

    Completed Day 67

---
+++ Java_Code/Java_01/Java_01_007/src/daySixySeven/SixtySeven_67.java
@@ -0,0 +1,52 @@
+package daySixySeven;
+
+public class SixtySeven_67 {
+    public static void main(String[] args) {
+        int a1 = 3;
+        System.out.println(numTrees(a1));
+        int[][] a2 = {{2, 1, 3}, {6, 5, 4}, {7, 8, 9}};
+        System.out.println(minFallingPathSum(a2));
+    }
+
+    private static final int INF = 1_000_000_000;
+
+    public static int minFallingPathSum(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] dp = new int[row][col];
+
+        for (int i = 0; i < col; i++) {
+            dp[0][i] = matrix[0][i];
+        }
+
+        int[] dif = {-1, 0, 1};
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int min = INF;
+                for (int d : dif) {
+                    if (j + d >= 0 && j + d < col) {
+                        min = Math.min(min, matrix[i][j] + dp[i - 1][j + d]);
+                    }
+                }
+                dp[i][j] = min;
+            }
+        }
+
+        int ans = INF;
+        for (int i = 0; i < col; i++) {
+            ans = Math.min(ans, dp[row - 1][i]);
+        }
+        return ans;
+    }
+
+    public static int numTrees(int n) {
+        int[] sol = new int[n + 1];
+        sol[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                sol[i] += sol[j] * sol[i - j - 1];
+            }
+        }
+        return sol[n];
+    }
+}
