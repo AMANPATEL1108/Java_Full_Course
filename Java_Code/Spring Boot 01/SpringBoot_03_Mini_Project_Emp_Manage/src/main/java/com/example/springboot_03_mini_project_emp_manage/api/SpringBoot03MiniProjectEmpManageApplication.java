edf36cdb7483b25f9fdbed3e3481fec3574abe6b
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-11-29 09:20:07 +0530

    Today is Day 83 and i have Setup  Mini Project of Empoyee Management System

---
+++ Java_Code/Spring Boot 01/SpringBoot_03_Mini_Project_Emp_Manage/src/main/java/com/example/springboot_03_mini_project_emp_manage/api/SpringBoot03MiniProjectEmpManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_03_mini_project_emp_manage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03MiniProjectEmpManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot03MiniProjectEmpManageApplication.class, args);
+	}
+
+}
---
+++ Java_Code/Spring Boot 01/SpringBoot_03_Mini_Project_Emp_Manage/src/test/java/com/example/springboot_03_mini_project_emp_manage/api/SpringBoot03MiniProjectEmpManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_03_mini_project_emp_manage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot03MiniProjectEmpManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
