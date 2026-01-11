47b557dca4d16794e6d23dcfb9baa69a539c1b04
AMANPATEL1108 <amanpatel2482@gmail.com>
2026-01-11 08:41:19 +0530

    Completed Day 126

---
+++ Java_Code/Spring Boot 01/SpringBoot_08/src/test/java/com/example/springboot_08/SpringBoot08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
