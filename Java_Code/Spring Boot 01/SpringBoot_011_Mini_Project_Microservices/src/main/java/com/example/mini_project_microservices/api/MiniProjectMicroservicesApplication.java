54a179b428649c284fa390fdb6549a68ca1da064
AMANPATEL1108 <amanpatel2482@gmail.com>
2026-01-16 05:43:18 +0530

    Completed Day 131

---
+++ Java_Code/Spring Boot 01/SpringBoot_011_Mini_Project_Microservices/src/main/java/com/example/mini_project_microservices/api/MiniProjectMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_project_microservices.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectMicroservicesApplication.class, args);
+	}
+
+}
