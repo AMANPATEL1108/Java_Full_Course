376fac216b895adc20f2915af22fb3eb6a4bf0df
AMANPATEL1108 <amanpatel2482@gmail.com>
2026-01-17 13:28:43 +0530

    Project Structured Completed

---
+++ Java_Code/Spring Boot 01/SpringBoot_011_Mini_Project_Microservices/Api_Gatway/src/test/java/com/example/Api_Gatway/api/ApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Api_Gatway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
