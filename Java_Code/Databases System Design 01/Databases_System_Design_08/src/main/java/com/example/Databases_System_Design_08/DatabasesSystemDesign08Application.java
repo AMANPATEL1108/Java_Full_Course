4d332546b60b4fecd0c07eabbd404f308a2acf93
AMANPATEL1108 <amanpatel2482@gmail.com>
2026-02-25 07:38:48 +0530

    Completed Day 171

---
+++ Java_Code/Databases System Design 01/Databases_System_Design_08/src/main/java/com/example/Databases_System_Design_08/DatabasesSystemDesign08Application.java
@@ -0,0 +1,13 @@
+package com.example.Databases_System_Design_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasesSystemDesign08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasesSystemDesign08Application.class, args);
+	}
+
+}
---
+++ Java_Code/Databases System Design 01/Databases_System_Design_08/src/test/java/com/example/Databases_System_Design_08/DatabasesSystemDesign08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Databases_System_Design_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasesSystemDesign08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
