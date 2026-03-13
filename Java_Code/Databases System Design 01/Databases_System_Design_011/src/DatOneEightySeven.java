44c84885baf7aad830d5c8ec1fc0ba12ba33afa2
AMANPATEL1108 <amanpatel2482@gmail.com>
2026-03-13 07:25:10 +0530

    Completed Day 187

---
+++ Java_Code/Databases System Design 01/Databases_System_Design_011/src/DatOneEightySeven.java
@@ -0,0 +1,16 @@
+public class DatOneEightySeven {
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("ZY"));
+    }
+
+    public static int titleToNumber(String columnTitle) {
+        int value = 0;
+
+        for (int i = 0; i < columnTitle.length(); i++) {
+            char letter = columnTitle.charAt(i);
+            value = value * 26 + (letter - 'A' + 1);
+        }
+
+        return value;
+    }
+}
