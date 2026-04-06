ad78b3ba9beb32cb7e8461a112497d8c978dc91b
AMANPATEL1108 <amanapatel2482@gmail.com>
2026-04-06 05:44:04 +0530

    Completed Day 211

---
+++ Java_Code/Cloud + DevOps 01/Cloud-Devops-01/src/main/java/com/cloud_01/api/Cloud_Devops_01/api/CloudDevops01Application.java
@@ -0,0 +1,13 @@
+package com.cloud_01.api.Cloud_Devops_01.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDevops01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDevops01Application.class, args);
+	}
+
+}
---
+++ Java_Code/Cloud + DevOps 01/Cloud-Devops-01/src/test/java/com/cloud_01/api/Cloud_Devops_01/api/CloudDevops01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_01.api.Cloud_Devops_01.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDevops01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
