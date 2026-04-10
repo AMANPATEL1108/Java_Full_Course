f2873ce0634742528916a828deb1e2c663c4734b
AMANPATEL1108 <amanapatel2482@gmail.com>
2026-04-10 07:14:09 +0530

    Completed Day 215

--- Java_Code/Cloud + DevOps 01/Cloud-Devops-02/src/main/java/com/example/Cloud_Devops_02/user/serviceImpl/UserServiceImpl.java
+++ Java_Code/Cloud + DevOps 01/Cloud-Devops-02/src/main/java/com/example/Cloud_Devops_02/user/serviceImpl/UserServiceImpl.java
@@ -19,24 +19,56 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-
     @Override
     public UserResponse createUser(UserRequest request) {
-        return null;
+
+        if (request.getEmail().isBlank() || request.getName().isBlank()) {
+            throw new IllegalArgumentException("Name and Email Must be Required");
+        }
+
+        User newUser = new User();
+        newUser.setEmail(request.getEmail());
+        newUser.setName(request.getName());
+
+        User saved = userRepository.save(newUser);
+
+        return userResponse(saved);
     }
 
     @Override
     public String deleteUser(Long id) {
-        return "";
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("user Not Found"));
+
+        userRepository.deleteById(id);
+        return "User Deleted Successfully";
     }
 
     @Override
     public List<UserResponse> getAllUser() {
-        return List.of();
+
+        List<User> users = userRepository.findAll();
+
+        return users.stream()
+                .map(this::userResponse)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<UserResponse> getUserById(Long id) {
-        return Optional.empty();
+
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("User Not Found"));
+
+        return Optional.of(userResponse(user));
+    }
+
+    UserResponse userResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setEmail(user.getEmail());
+        response.setName(user.getName());
+
+        return response;
     }
 }
